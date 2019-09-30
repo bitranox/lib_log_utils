4e308bc730130246a07211121138365068738d87
bitranox <1uGToNSiYo85pWhQcqFu>
2019-09-30 20:58:23 +0200

    refractor

--- lib_log_utils/lib_log_utils.py
+++ lib_log_utils/lib_log_utils.py
@@ -1,4 +1,5 @@
 # STDLIB
+import errno
 # noinspection PyUnresolvedReferences
 import getpass
 # noinspection PyUnresolvedReferences
@@ -24,22 +25,37 @@ except ImportError:                 # type: ignore # pragma: no cover
 
 
 def main() -> None:
-    BannerSettings.called_via_commandline = True
-    # we must not call fire if the program is called via pytest
-    is_called_via_pytest = [(sys_arg != '') for sys_arg in sys.argv if 'pytest' in sys_arg]
-    if not is_called_via_pytest:
-        fire.Fire({
-            'banner_color_test': banner_color_test,
-            'banner_spam': banner_spam,
-            'banner_debug': banner_debug,
-            'banner_verbose': banner_verbose,
-            'banner_info': banner_info,
-            'banner_notice': banner_notice,
-            'banner_success': banner_success,
-            'banner_warning': banner_warning,
-            'banner_error': banner_error,
-            'banner_critical': banner_critical,
-        })
+    try:
+        BannerSettings.called_via_commandline = True
+        # we must not call fire if the program is called via pytest
+        is_called_via_pytest = [(sys_arg != '') for sys_arg in sys.argv if 'pytest' in sys_arg]
+        if not is_called_via_pytest:
+            fire.Fire({
+                'banner_color_test': banner_color_test,
+                'banner_spam': banner_spam,
+                'banner_debug': banner_debug,
+                'banner_verbose': banner_verbose,
+                'banner_info': banner_info,
+                'banner_notice': banner_notice,
+                'banner_success': banner_success,
+                'banner_warning': banner_warning,
+                'banner_error': banner_error,
+                'banner_critical': banner_critical,
+            })
+
+    except FileNotFoundError:
+        # see https://www.thegeekstuff.com/2010/10/linux-error-codes for error codes
+        # No such file or directory
+        sys.exit(errno.ENOENT)      # pragma: no cover
+    except FileExistsError:
+        # File exists
+        sys.exit(errno.EEXIST)      # pragma: no cover
+    except TypeError:
+        # Invalid Argument
+        sys.exit(errno.EINVAL)      # pragma: no cover
+        # Invalid Argument
+    except ValueError:
+        sys.exit(errno.EINVAL)      # pragma: no cover
 
 
 if __name__ == '__main__':
