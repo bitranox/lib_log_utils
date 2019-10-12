0eb4f930bdd60d3cd1677fcaf4a040a2bc3fc860
bitranox <rnowotny1966@gmail.com>
2019-10-12 23:16:11 +0200

    refractor

--- lib_log_utils/log_handlers.py
+++ lib_log_utils/log_handlers.py
@@ -5,7 +5,7 @@ import getpass
 import os
 import platform
 import sys
-from typing import Any, Optional
+from typing import Any, Dict, Optional
 
 # EXT
 import coloredlogs      # type: ignore
@@ -67,8 +67,8 @@ def add_stream_handler_color(logger: logging.Logger = logging.getLogger(),
                              level: int = logging.INFO,
                              fmt: str = '[{username}@%(hostname)s][%(asctime)s][%(levelname)-8s]: %(message)s',
                              datefmt: str = '%Y-%m-%d %H:%M:%S',
-                             field_styles: Any = coloredlogs.DEFAULT_FIELD_STYLES,
-                             level_styles: Any = coloredlogs.DEFAULT_LEVEL_STYLES,
+                             field_styles: Dict[str, Dict[str, Any]] = coloredlogs.DEFAULT_FIELD_STYLES,
+                             level_styles: Dict[str, Dict[str, Any]] = coloredlogs.DEFAULT_LEVEL_STYLES,
                              remove_existing_handlers: bool = True) -> logging.Handler:
     """
     # https://coloredlogs.readthedocs.io/en/latest/api.html
@@ -86,6 +86,7 @@ def add_stream_handler_color(logger: logging.Logger = logging.getLogger(),
         remove_all_handlers(logger=logger)
 
     if not exists_handler_with_name(name):
+        set_colored_log_environment_variables_if_not_set(fmt, datefmt, field_styles, level_styles)    # for travis / xenial we need to set environment variables
         fmt = override_via_environment(fmt, 'COLOREDLOGS_LOG_FORMAT')
         if hasattr(fmt, 'format'):
             fmt = fmt.format(username=getpass.getuser())
@@ -250,3 +251,66 @@ def set_log_handler_formatter_prefix(handler: logging.Handler, log_formatter_pre
         datefmt = '%Y-%m-%d %H:%M:%S'
         formatter = logging.Formatter(log_formatter_prefix + '%(message)s', datefmt)
         handler.setFormatter(formatter)
+
+
+def set_colored_log_environment_variables_if_not_set(fmt: str, datefmt: str, field_styles: Dict[str, Dict[str, Any]], level_styles: Dict[str, Dict[str, Any]]) -> None:
+    """
+    for travis / xenial we need to set environment variables, or colored logs does not work (on travis, linux xenial)
+    if the environment variables are set already, they wont be overwritten
+    """
+    if not is_environment_variable_set('COLOREDLOGS_LOG_FORMAT'):
+        os.environ['COLOREDLOGS_LOG_FORMAT'] = fmt
+    if not is_environment_variable_set('COLOREDLOGS_DATE_FORMAT'):
+        os.environ['COLOREDLOGS_DATE_FORMAT'] = datefmt
+    if not is_environment_variable_set('COLOREDLOGS_FIELD_STYLES'):
+        os.environ['COLOREDLOGS_FIELD_STYLES'] = convert_styles_to_text(field_styles)
+    if not is_environment_variable_set('COLOREDLOGS_LEVEL_STYLES'):
+        os.environ['COLOREDLOGS_LEVEL_STYLES'] = convert_styles_to_text(level_styles)
+
+
+def is_environment_variable_set(env_variable: str) -> bool:
+    env_variable_set = False
+    if env_variable in os.environ:
+        if str(os.environ[env_variable]).strip():
+            env_variable_set = True
+    return env_variable_set
+
+
+def convert_styles_to_text(style_dict: Dict[str, Dict[str, Any]]):
+    """
+
+    >>> field_styles = {'asctime': {'color': 'green'},'hostname': {'color': 'green'},'levelname': {'color': 'yellow'},
+    ...                 'name': {'color': 'blue'},'programname': {'color': 'cyan'}}
+    >>> level_styles = {'spam': {'color': 'magenta', 'bright': True},'debug': {'color': 'blue', 'bright': True},
+    ...                 'verbose': {'background': 'blue', 'bright': True},'info': {},'notice': {'background': 'magenta', 'bright': True},
+    ...                 'warning': {'color': 'red', 'bright': True},'success': {'color': 'green', 'bright': True},
+    ...                 'error': {'background': 'red', 'bright': True},'critical': {'background': 'red'}}
+
+    >>> result = convert_styles_to_text(field_styles)
+    >>> assert result == 'asctime=color=green;hostname=color=green;levelname=color=yellow;name=color=blue;programname=color=cyan'
+    >>> assert result == convert_styles_to_text(coloredlogs.parse_encoded_styles(result))
+
+    >>> result = convert_styles_to_text(level_styles)
+    >>> assert result.startswith('critical=background=red;debug=bright,color=blue;error=background=red,bright;info=;notice=background=magenta,')
+    >>> assert result == convert_styles_to_text(coloredlogs.parse_encoded_styles(result))
+    """
+    l_styles = list()
+    for style in style_dict:
+        l_styles.append(str(style) + '=' + dict_to_string(style_dict[style]))
+    l_styles.sort()
+    str_styles = ';'.join(l_styles)
+    return str_styles
+
+
+def dict_to_string(val_dict: Dict[str, Any], seperator: str = ',') -> str:
+    """
+    >>> dict_to_string({'background': 'blue', 'bright': True})
+    'background=blue,bright'
+    """
+    l_str = list()
+    for key in val_dict:
+        l_str.append(str(key) + '=' + str(val_dict[key]).lower())
+    l_str.sort()
+    val_str = seperator.join(l_str)
+    val_str = val_str.replace('=true', '')
+    return val_str
